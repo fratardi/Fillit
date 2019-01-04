d44f116c5869842860c5099b66f6c455d92e07ac
Francois TARDIEU <fratardi@e1r13p4.42.fr>
2019-01-04 18:45:48 +0100

    is within line edited

---
+++ src/isgridvalid.c
@@ -0,0 +1,42 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   isgridvalid.c                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/01/04 17:32:13 by fratardi          #+#    #+#             */
+/*   Updated: 2019/01/04 18:45:14 by fratardi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+unsigned short 	tetriscolmuv(unsigned short form)
+{
+//	apply mask of the seleccted line  (hold the content of the line )
+}
+
+unsigned short  aggregatelines(unsigned short line, unsigned short (rez of tetriscolmuv))
+{
+	
+}
+
+int		within the line(unsigned short linetocheck, int	gridsize)
+{
+		return ((linetocheck>>gridsize) > 0);
+}
+
+int		isgridvalid(t_tetris *tetris)
+{
+	
+
+
+	while()
+	{
+	}
+	
+
+
+
+
+	return (0);
+}
