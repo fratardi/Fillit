dce413190e9f86da2d09fdbbacabb42383423a2f
Francois TARDIEU <fratardi@e2r5p1.42.fr>
2019-01-06 15:12:36 +0100

    edited logic for test

--- src/isgridvalid.c
+++ src/isgridvalid.c
@@ -6,7 +6,7 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 17:32:13 by fratardi          #+#    #+#             */
-/*   Updated: 2019/01/04 18:45:14 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/01/05 18:57:30 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,9 +15,15 @@ unsigned short 	tetriscolmuv(unsigned short form)
 //	apply mask of the seleccted line  (hold the content of the line )
 }
 
-unsigned short  aggregatelines(unsigned short line, unsigned short (rez of tetriscolmuv))
+unsigned short  filllinex(s_struct *tetri)
 {
-	
+	unsigned short	line;
+
+
+
+
+
+	line  = 0
 }
 
 int		within the line(unsigned short linetocheck, int	gridsize)
@@ -27,16 +33,8 @@ int		within the line(unsigned short linetocheck, int	gridsize)
 
 int		isgridvalid(t_tetris *tetris)
 {
-	
-
-
 	while()
 	{
 	}
-	
-
-
-
-
 	return (0);
 }
