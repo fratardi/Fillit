5a460a2fb6980f156fb7224e790f65029762d3b3
Francois TARDIEU <fratardi@e2r5p1.42.fr>
2019-01-06 17:59:23 +0100

    improvements to the gridchecker

--- src/isgridvalid.c
+++ src/isgridvalid.c
@@ -6,32 +6,46 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/04 17:32:13 by fratardi          #+#    #+#             */
-/*   Updated: 2019/01/05 18:57:30 by fratardi         ###   ########.fr       */
+/*   Updated: 2019/01/06 17:59:05 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-unsigned short 	tetriscolmuv(unsigned short form)
+
+
+//function that gets the content of a tetrimino line and moves the x position to get it right
+unsigned short 	tetrilinejuster(unsigned short tetris, int  linetoget, int position)
 {
-//	apply mask of the seleccted line  (hold the content of the line )
+	if(linetoget == 0)
+		return ((tetris & 61440)>>position);
+	if (linetoget == 1)
+		return (((tetris & 3840)<<4)>>position);
+	if(linetoget == 2)
+		return (((tetris & 240)<<8)>>position);
+	if(linetoget == 4)
+		return (((tetris & 15)<<12)>>position);
 }
 
-unsigned short  filllinex(s_struct *tetri)
+unsigned short  filllinex(s_struct *tetri,int linetocheck)
 {
-	unsigned short	line;
-
-
-
-
-
-	line  = 0
+	unsigned short line;
+	
+	line = 0;
+	
+	while(tetri->next != NULL)
+	{
+			
+	}
 }
 
-int		within the line(unsigned short linetocheck, int	gridsize)
+int		within_line_border(unsigned short linetocheck, int	gridsize)
 {
-		return ((linetocheck>>gridsize) > 0);
+	unsigned short		mask;
+
+	mask = 65535;
+	return (((mask = mask>>gridsize) > 0 ) ? 0 : 1);
 }
 
-int		isgridvalid(t_tetris *tetris)
+int		isgridvalid(t_tetris *tetris,int gridsize)
 {
 	while()
 	{
